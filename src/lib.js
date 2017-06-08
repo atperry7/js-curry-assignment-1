a8c69e8abe6f5017dba34e377d6a2a14d15a4f57
Anthony Perry <atperry7@gmail.com>
2017-06-07 21:06:53 -0500

    Implemented the imperative way, output matches correctly

--- src/lib.js
+++ src/lib.js
@@ -19,13 +19,44 @@ const listedPrice =
         ? listing.price
         : 0
 
+const itemChecker = (list, item) => {
+  return list.name === item ? listing.price : 0
+}
+
+
+const customerItemsTotal = (listings, items) => {
+  let total = 0
+  for (var i = 0; i < listings.length; i++) {
+    for (var j = 0; j < items.length; j++) {
+       if (listings[i].name === items[j]) {
+         total = total + listings[i].price
+       }
+    }
+  }
+
+  return total
+}
+
+const customerObjectCreater = (cust, listings) => {
+  let customerObject = {name: '', total: 0}
+  let total = customerItemsTotal(listings, cust.items)
+  customerObject.name = cust.customer
+  customerObject.total = total
+  return customerObject
+}
+
+
 /**
  * transform carts into an array of { customer, total }
  */
 const calculateTotals =
   listings =>
     carts => {
-      console.log('Hello')
+      const customers = []
+      for (let cart of carts) {
+        customers.push(customerObjectCreater(cart, listings))
+      }
+      return customers
     }
 
 module.exports = {
