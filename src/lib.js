a3f2b008d1cfd799415c04d6c64f4e7c8a9a29bc
Anthony Perry <atperry7@gmail.com>
2017-06-11 16:27:18 -0500

    Added listingsBreakDown function and cleaned up syntax

--- src/lib.js
+++ src/lib.js
@@ -19,17 +19,21 @@ const listedPrice =
         ? listing.price
         : 0
 
+const listingsBreakDown =
+  listings =>
+    item =>
+       listings.reduce((total, current) => { return total + listedPrice(current)(item) }, 0)
+
 /**
  * transform carts into an array of { customer, total }
  */
 const calculateTotals =
   listings =>
     carts => {
-      const listingsTest = listedPrice(listings)
-      return carts.map((custo) => { return { customer: custo.customer, total: custo.items.reduce((total, item) =>
-        { return total + listings.reduce((total, listing) =>
-          { return total + listedPrice(listing)(item) }, 0) }, 0) }
-      })
+      const listingsTest = listingsBreakDown(listings)
+
+      return carts.map((custo) =>
+      { return { customer: custo.customer, total: custo.items.reduce((total, item) => { return total + listingsTest(item) }, 0) } })
     }
 
 module.exports = {
