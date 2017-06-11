ca694c335fe97fead484064b5b2a2989e51f3442
Anthony Perry <atperry7@gmail.com>
2017-06-11 16:11:19 -0500

    Refactored closer to a functional way

--- src/lib.js
+++ src/lib.js
@@ -19,47 +19,17 @@ const listedPrice =
         ? listing.price
         : 0
 
-const customerItemsTotal = (listings, items) => {
-  let total = 0
-  for (var i = 0; i < listings.length; i++) {
-    for (var j = 0; j < items.length; j++) {
-      if (listings[i].name === items[j]) {
-        total = total + listings[i].price
-      }
-    }
-  }
-
-  return total
-}
-
-const figureOutTotal = (listing, items) => {
-  const listingTest = listedPrice(listing)
-  let total
-  let totals = 0
-  for (let item of items) {
-    total = Number(listingTest(item))
-    totals = totals + total
-  }
-  return totals
-}
-
-const customerObjectCreater = (cust, listings) => {
-  let customerObject = {total: 0}
-  customerObject = cust
-  let aNewTotal = listings.reduce(list => figureOutTotal(list, cust.items))
-
-  let total = customerItemsTotal(listings, cust.items)
-  customerObject.total = total
-  return customerObject
-}
-
 /**
  * transform carts into an array of { customer, total }
  */
 const calculateTotals =
   listings =>
     carts => {
-      return carts.map((custo) => { return customerObjectCreater(custo, listings) })
+      const listingsTest = listedPrice(listings)
+      return carts.map((custo) => { return { customer: custo.customer, total: custo.items.reduce((total, item) =>
+        { return total + listings.reduce((total, listing) =>
+          { return total + listedPrice(listing)(item) }, 0) }, 0) }
+      })
     }
 
 module.exports = {
